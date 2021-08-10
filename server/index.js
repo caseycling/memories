870412691c9df80e1ac18d09b2a5bfd58a3fc250
Casey <caseyclinga@gmail.com>
2021-08-10 16:13:19 -0700

    Server created with successful connection to db

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+//
+const CONNECTION_URL =
+  'mongodb+srv://caseycling:123abc@passport.czhpd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((err) => console.log(err));
+
+mongoose.set('useFindAndModify', false);
