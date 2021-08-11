338c1a7390990dfa27501013f622f262ed131abf
Casey <caseyclinga@gmail.com>
2021-08-11 12:21:00 -0700

    Rearranged folder structure

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,8 @@
 import express from 'express';
+
+//Destructure for individual named exports
 import { getPosts } from '../controllers/posts.js';
+
 const router = express.Router();
 
 router.get('/', getPosts);
