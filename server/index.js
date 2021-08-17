49b72ca7f109dc4885f334d5b31cba022637ea2d
Casey <caseyclinga@gmail.com>
2021-08-17 11:15:09 -0700

    Removed unnecessary bodyparser dependency

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
@@ -7,14 +6,15 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(express.json({ limit: '30mb' }));
+app.use(express.urlencoded({ limit: '25mb', extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
 
 const CONNECTION_URL =
   'mongodb+srv://caseycling:123abc@passport.czhpd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
