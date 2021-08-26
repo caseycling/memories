ac7789a959df0eb3c234a542297ea9bc63aefb0c
Casey <caseyclinga@gmail.com>
2021-08-26 14:16:09 -0700

    Added functionality for updating and deleting posts

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,18 @@
 import express from 'express';
 
 //Destructure for individual named exports
-import { getPosts, createPost } from '../controllers/posts.js';
+import {
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost,
+} from '../controllers/posts.js';
 
 const router = express.Router();
 
 router.get('/', getPosts);
 router.post('/', createPost);
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
 
 export default router;
